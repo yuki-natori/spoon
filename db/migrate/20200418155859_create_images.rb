10733e76315a2dd96800c475269eb3156d8c126d
yuki-natori <61723072+yuki-natori@users.noreply.github.com>
2020-04-19 09:10:39 +0900

    db 削除

--- db/migrate/20200418155859_create_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateImages < ActiveRecord::Migration[5.2]
-  def change
-    create_table :images do |t|
-      t.string :name,     null: false
-      t.references :item, null: false, index: true, foreign_key: true
-      t.timestamps
-    end
-  end
-end
