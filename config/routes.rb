cb946d53a4448392be1342892d0c3a3f52b220f0
yuki-natori <61723072+yuki-natori@users.noreply.github.com>
2020-05-12 12:25:02 +0900

    登録機能完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+  end
+
   root 'main#index'
+  
   resources :items
 end
